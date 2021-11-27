1cfd6602307dce04aed1fa5b1cb7424748dee718
Jeff Francis <jeff@gritch.org>
2021-11-27 09:57:33 -0700

    added real command line parameters

--- example.py
+++ example.py
@@ -3,24 +3,30 @@
 
 import sys
 import time
+import argparse
 from js8net import *
 
 # Main program.
 if __name__ == '__main__':
-    start_net("10.1.1.141",2442)
+    parser=argparse.ArgumentParser(description="Send grid square to APRS.")
+    parser.add_argument("--js8_host",default="localhost",help="IP/DNS of JS8Call server (default localhost)")
+    parser.add_argument("--js8_port",default=2442,help="TCP port of JS8Call server (default 2442)")
+    args=parser.parse_args()
+
+    start_net(args.js8_host,args.js8_port)
     time.sleep(1)
     print("Call: ",get_callsign())
     print("Grid: ",get_grid())
     print("Info: ",get_info())
     print("Freq: ",get_freq())
     print("Speed: ",get_speed())
-#    print(get_tx_text())
-#    print(get_rx_text())
     print("Freq: ",set_freq(7078000,2000))
     get_band_activity()
     
     while(True):
         if(not(rx_queue.empty())):
             with rx_lock:
-                print(rx_queue.get())
+                rx=rx_queue.get()
+                if(rx['type']=="RX.DIRECTED"):
+                    print(json.dumps(rx,indent=2,sort_keys=True))
             time.sleep(0.1)

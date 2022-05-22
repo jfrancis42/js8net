34fe6384249240775648b20b921fd7c0c765af74
EchoJuliet-01 <eljefe31415@gmail.com>
2022-05-21 18:11:54 -0600

    Fixed some string/integer issues.

--- send_grid.py
+++ send_grid.py
@@ -123,7 +123,7 @@ if __name__ == "__main__":
         js8host="localhost"
 
     if(args.js8_port):
-        js8port=args.js8_port
+        js8port=int(args.js8_port)
     elif(os.environ.get("JS8PORT") and args.env):
         js8port=int(os.environ.get("JS8PORT"))
     else:
@@ -140,7 +140,7 @@ if __name__ == "__main__":
         gpsdhost="localhost"
 
     if(args.gpsd_port):
-        gpsdport=args.gpsd_port
+        gpsdport=int(args.gpsd_port)
     elif(os.environ.get("GPSDPORT") and args.env):
         gpsdport=int(os.environ.get("GPSDPORT"))
     else:
@@ -154,8 +154,8 @@ if __name__ == "__main__":
     grid=False
     if(args.freq or args.freq_dial or args.freq_audio):
         f=get_freq()
-        if(args.dial_freq and args.freq_audio):
-            set_freq(args.dial_freq,args.freq_audio)
+        if(args.freq_dial and args.freq_audio):
+            set_freq(args.freq_dial,args.freq_audio)
         elif(args.freq and args.freq_audio):
             set_freq(args.freq_dial-args.freq_audio,args.freq_audio)
         elif(args.freq):
@@ -165,10 +165,11 @@ if __name__ == "__main__":
     if(args.verbose):
         print("Frequency set to ",get_freq())
     if(args.speed):
-        if(args.speed>=0 and args.speed<=4 and args.speed!=3):
+        speed=int(args.speed)
+        if(speed>=0 and speed<=4 and speed!=3):
             if(args.verbose):
-                print("Setting speed to ",str(args.speed))
-            set_speed(args.speed)
+                print("Setting speed to ",str(speed))
+            set_speed(speed)
     if(args.get_grid):
         grid=get_grid()
     if(args.lat and args.lon):
